e5e6add1ef2fe3939540cbeb90cc77ab890e9e05
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2025-02-04 20:07:32 +0300

    commit

--- app/home/portefeuille/_layout.tsx
+++ app/home/portefeuille/_layout.tsx
@@ -6,7 +6,7 @@ const _layout = () => {
 	return (
 		<Stack>
 			<Stack.Screen name='index' options={{headerShown : false}}/>
-			<Stack.Screen name='[[id]]' options={{headerShown : false}}/>
+			<Stack.Screen name='[id]' options={{headerShown : false}}/>
 		</Stack>
 	)
 }
