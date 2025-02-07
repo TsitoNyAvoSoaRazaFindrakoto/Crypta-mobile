bd55a8669de44ef05aeb6af2d48932fa13a0fc81
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2025-02-07 17:10:22 +0300

    added .env but for expo

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -3,6 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
+import Constants from 'expo-constants';
+
+const { extra } = Constants.expoConfig;
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,13 +13,13 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_dhXrU5-3m_QsUAka7FVavlGTgNTlppI",
-  authDomain: "crypta-d5e13.firebaseapp.com",
-  projectId: "crypta-d5e13",
-  storageBucket: "crypta-d5e13.firebasestorage.app",
-  messagingSenderId: "539604836728",
-  appId: "1:539604836728:web:5876a760ea6bf2189ee88d",
-  measurementId: "G-X7J7VJSX4N",
+  apiKey: expo.firebase.apiKey,
+  authDomain: expo.firebase.authDomain,
+  projectId: expo.firebase.projectId,
+  storageBucket: expo.firebase.storageBucket,
+  messagingSenderId: expo.firebase.messagingSenderId,
+  appId: expo.firebase.appId,
+  measurementId: expo.firebase.endpointUrl,
 };
 
 // Initialize Firebase
